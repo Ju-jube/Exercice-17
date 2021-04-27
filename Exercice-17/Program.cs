e5ea5bdb75fc0b1e9c9a772ecc4e24b61a00eaa4
Justine Pic <justine.pic@outlook.fr>
2021-04-27 13:17:17 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice-17/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choix;
+            do
+            {
+
+                
+
+
+                // saisir un nombre
+                Console.Write("Saisir un nombre = ");
+                int nombre = int.Parse(Console.ReadLine());
+
+                //saisir la racine carrée du nombre
+                Console.Write("Saisir la racine carrée de " + nombre + " = ");
+                double racine = double.Parse(Console.ReadLine());
+
+                //bonne réponse : message bravo
+                double reponse = Math.Sqrt(nombre);
+                if (racine == reponse)
+                {
+                    Console.WriteLine("Bravo ! ");
+                }
+                //mauvaise réponse : message erreur + bonne réponse
+                else
+                {
+                    Console.Write("Erreur, la racine carrée de " + nombre + " est = " + reponse);
+                }
+
+                Console.WriteLine("Entrer une autre nombre ? (O/N)");
+                choix = Console.ReadLine();
+                Console.Clear();
+                
+
+            } while (choix == "O");
+        }
+    }
+}
